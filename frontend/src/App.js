f18cf65909fc0d1098de9c23d85c5811bd9a795c
kev737 <kevin.heffernan97@gmail.com>
2021-05-17 00:31:10 -0700

    kh - branding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassicsTable from './components/ClassicsTable';
+import { Navbar } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
+      <Navbar>
+        <Navbar.Brand href="/">Old Timey</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            A Site for Sore Eyes
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
       <ClassicsTable/>
+      <Navbar>
+        <Navbar.Brand href="/"></Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Made for Dad!
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
